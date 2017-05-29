cdfe346356443ab9c92d79a568703b60f81a371f
dedyrosyadi <dedy.rosyadi@gmail.com>
2017-05-29 17:06:25 +0800

    edit animate scroll

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12631,40 +12631,18 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 // console.log("gulp bootstrap starter");
+$('.page-scroll').on('click', function(e) {
+	var target = $(this).attr('href');
 
-$(document).ready(function(){
+	// $('body').scrollTop(target.offset().top);
+	$('body').animate({
+        scrollTop: $(target).offset().top
+    }, 800);
 
-	$('.go-top').click(function(){
-		$('html, body').animate({scrollTop : 0},1000);
-		return false;
-	});
-
-});
+	e.preventDefault();
 
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $('.scroll').on('click', function(event) {
+});	
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
 
 // console.log('@muslihzarth');
