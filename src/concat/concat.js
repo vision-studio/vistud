62f858c448ce4b90b372aca937d1ef955fb94123
dedyrosyadi <dedy.rosyadi@gmail.com>
2017-09-15 02:04:15 +0800

    update menu resv

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12636,20 +12636,26 @@ $('.page-scroll').on('click', function(e) {
 
 	// $('body').scrollTop(target.offset().top);
 	$('body').animate({
-        scrollTop: $(target).offset().top
+        scrollTop: $(target).offset().top - 60
     }, 800);
 
 	e.preventDefault();
 });	
 
 
-
 function openNav() {
-    document.getElementById("menu-resv").style.width = "100%";
+    // document.getElementById("menu-resv").style.opacity = 1;	
+    // document.getElementById("menu-resv").style.display = "block";
+    // document.getElementById("menu-resv").style.width = "100%";
+    // $("#menu-resv").css({'animation': 'fadein .5s'})
+    $("#menu-resv").animate({height: "100%"});
 }
 
 function closeNav() {
-    document.getElementById("menu-resv").style.width = "0%";
+    // document.getElementById("menu-resv").style.width = "0%";
+    // document.getElementById("menu-resv").style.opacity = 0;
+    // document.getElementById("menu-resv").style.display = "none";
+    $("#menu-resv").animate({height: "0"});
 }
 
 
