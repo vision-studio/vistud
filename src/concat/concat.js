b7ba1f3d34b443b10b84f6ca4a0921cd0f63b8c8
dedyrosyadi <dedy.rosyadi@gmail.com>
2018-10-16 01:39:45 +0800

    update icon and style

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12669,6 +12669,18 @@ $(document).on("scroll", function() {
 });
 
 
+$('img').each(function() {
+var $img = $(this);
+var imgURL = $img.attr('src');
+
+$.get(imgURL, function(data) {
+  // Get the SVG tag, ignore the rest
+  var $svg = $(data).find('svg');
+  // Replace image with new SVG
+  $img.replaceWith($svg);
+});
+});
+
 
 
 // console.log('@muslihzarth');
--- src/js/main.js
+++ src/js/main.js
@@ -37,4 +37,16 @@ $(document).on("scroll", function() {
 });
 
 
+$('img').each(function() {
+var $img = $(this);
+var imgURL = $img.attr('src');
+
+$.get(imgURL, function(data) {
+  // Get the SVG tag, ignore the rest
+  var $svg = $(data).find('svg');
+  // Replace image with new SVG
+  $img.replaceWith($svg);
+});
+});
+
 
