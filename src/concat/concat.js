12a4d9fa6039c2df006eeaaa6a50c423851edad5
dedyrosyadi <dedy.rosyadi@gmail.com>
2018-12-12 11:37:48 +0800

    update resv

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12631,43 +12631,53 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 console.log("Vision Studio - 2018");
+
 $('.page-scroll').on('click', function(e) {
   var target = $(this).attr('href');
 
   $('html, body').animate({
-    scrollTop: $(target).offset().top - 60
+  scrollTop: $(target).offset().top - 60
   }, 800);
   e.preventDefault();
 }); 
 
 $(document).ready(function(){
   $('#nav-icon').click(function(){
-    $(this).toggleClass('animate-icon');
-		$('#overlay').fadeToggle();
+  $(this).toggleClass('animate-icon');
+    $('#overlay').fadeToggle();
   });
 });
 
 $(document).ready(function(){
   $('a.menu').click(function(){
-		$('#nav-icon').removeClass('animate-icon');
-		if ($(window).width() <= 720) {
-			$('#overlay').toggle();		
-		}
-		
+    $('#nav-icon').removeClass('animate-icon');
+    if ($(window).width() <= 720) {
+      $('#overlay').toggle();   
+    }
   });
 });
 
 $(document).on("scroll", function() {
-	if($(document).scrollTop()>100) {
-		$("header").css({"background-color":"rgba(0, 0, 0, .8)",
-			"box-shadow":"0px 2px 10px 0px #2982bf"});
-	}
-	else {
-		$("header").css({"background-color":"rgba(0, 0, 0, 0)",
-			"box-shadow":"none"});
-	}
+  if($(document).scrollTop()>100) {
+    $("header").css({"background-color":"rgba(0, 0, 0, .8)",
+      "box-shadow":"0px 2px 10px 0px #2982bf"});
+  }
+  else {
+    $("header").css({"background-color":"rgba(0, 0, 0, 0)",
+      "box-shadow":"none"});
+  }
 });
 
+$(window).resize(function() {
+  if ($(window).width() > 720) {
+     // $('#overlay').toggle();
+    $('#overlay').css({"display":"block"});
+  }
+ else {
+    $('#nav-icon').removeClass('animate-icon');
+    $('#overlay').css({"display":"none"});
+ }
+});
 
 $('img').each(function() {
 var $img = $(this);
@@ -12682,5 +12692,3 @@ $.get(imgURL, function(data) {
 });
 
 
-
-// console.log('@muslihzarth');
